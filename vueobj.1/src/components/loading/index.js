3a233a92dc437d55b26c01657e7e5b4d5ccf8a16
dukeke0629 <m18838258363@163.com>
2019-01-22 09:17:24 +0800

    这是杜科伟的vue项目

--- vueobj.1/src/components/loading/index.js
+++
@@ -1,7 +0,0 @@
-//主角
-
-import loading from './components/loading.vue';
-
-export default function(Vue){
-  Vue.component('loading',loading);//注册到全局
-}
--- vueobj.1/src/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-
-import axios from 'axios' ; 
-
-export default{
-    install:function(Vue){
-        window.axios = axios;   
-        Vue.prototype.$http = axios;//全局暴露
-    }
-}
